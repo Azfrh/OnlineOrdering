58f7e1f2fb80a4ab4ecbf7951af2c3b0c52ee6fa
Azfrh <2020897568@student.uitm.edu.my>
2023-04-01 04:03:46 +0800

    register

--- register.php
+++ register.php
@@ -13,7 +13,7 @@
 
 <body style="background-color:hsla(0, 100%, 50%, 0.1);">
 
-    <img src="Vegan Chocolate Cake With Whipped Ganache.jpg" alt="" width="200" height="200">
+    <img src="Vegan Chocolate Cake With Whipped Ganache.jpg" alt="" width="48" height="48">>
 
     <div class="wrapper">
         <h2>Register Account</h2>
