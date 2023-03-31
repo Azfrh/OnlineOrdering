e98d04e91f5918a592892fb5378685831f76be05
Azfrh <2020897568@student.uitm.edu.my>
2023-04-01 03:55:29 +0800

    loginpage

--- Loginpage.php
+++ Loginpage.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="mystyle.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; }
         .wrapper{ width: 360px; padding: 20px; border:2px solid Black; }
