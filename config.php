39f42b0f8a8912bdcb9bf80f62e4aa9c5f494cdc
Azfrh <2020897568@student.uitm.edu.my>
2023-04-01 06:49:17 +0800

    commit

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname= "db";
+$dbname= "onlineordering";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
