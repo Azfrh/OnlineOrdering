ab57f2bc115abe57d1bc6e277d950c7126f95651
Azfrh <2020897568@student.uitm.edu.my>
2023-04-01 04:12:04 +0800

    register

--- register.php
+++ register.php
@@ -7,7 +7,8 @@
     <style>
         body{ font: 14px verdana; } 
         body{display:flex; flex-direction:column; justify-content:center; align-items:center;}
-        .wrapper{ width: 360px; padding: 20px; border:2px solid Black; }   
+        .wrapper{ width: 360px; padding: 20px; border:2px solid Black; } 
+            background-image: url('img_girl.jpg');
     </style>
 </head>
 
