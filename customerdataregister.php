48a2250eaac24260eb51719ef869f52a1de3a2f9
Azfrh <2020897568@student.uitm.edu.my>
2023-04-08 10:24:13 +0800

    commit

--- customerdataregister.php
+++ customerdataregister.php
@@ -2,7 +2,12 @@
 error_reporting(E_ALL); 
 ini_set('display_errors', 1); 
 include 'config.php'; 
- 
+
+$id = filter_input(INPUT_POST, 'customer_id'); 
+$fullname = filter_input(INPUT_POST, 'customer_name');  
+$address = filter_input(INPUT_POST, 'customer_address'); 
+$phone = filter_input(INPUT_POST, 'customer_phone'); 
+$password = filter_input(INPUT_POST, 'customer_password'); 
 $sql="INSERT INTO customer (customer_id, customer_name, customer_address, customer_phone, customer_password) 
 values ('$_POST[customer_id]','$_POST[customer_name]','$_POST[customer_address]','$_POST[customer_phone]','$_POST[customer_password]')"; 
  if (!mysqli_query($conn,$sql)){ 
@@ -12,6 +17,6 @@ values ('$_POST[customer_id]','$_POST[customer_name]','$_POST[customer_address]'
  
  
 echo "<script 
-type='text/jscript'>alert('Register agent has been submitted!')</script>"; 
+type='text/jscript'>alert('Register customer has been submitted!')</script>"; 
 header('refresh:1 url=register.php'); 
 ?>
