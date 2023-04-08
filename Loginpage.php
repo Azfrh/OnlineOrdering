3097b5c1c39c0dae5c4f4baa2eec6d524174feb5
Azfrh <2020897568@student.uitm.edu.my>
2023-04-08 10:44:40 +0800

    commit

--- Loginpage.php
+++ Loginpage.php
@@ -4,7 +4,7 @@ session_start();
  
 include "config.php"; 
  
-if (isset($_POST['uname']) && isset($_POST['password'])) { 
+if (isset($_POST['customer_id']) && isset($_POST['customer_password'])) { 
  
     function validate($data){ 
        $data = trim($data); 
@@ -13,32 +13,31 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
        return $data; 
     } 
  
-    $uname = validate($_POST['uname']); 
-    $pass = validate($_POST['password']); 
+    $customer_id = validate($_POST['customer_id']); 
+    $customer_password = validate($_POST['customer_password']); 
  
-    if (empty($uname)) { 
-        header("Location: index.php?error=Email is required"); 
+    if (empty($customer_id)) { 
+        header("Location: customerdataregister.php?error=Email is required"); 
         exit(); 
     } 
-    else if(empty($pass)){ 
-        header("Location: index.php?error=Password is required"); 
+    else if(empty($customer_password)){ 
+        header("Location: customerdataregister.php ?error=Password is required"); 
         exit(); 
     } 
     else{ 
  
-        $sql = "SELECT * FROM customer WHERE customer_email='$uname' AND customer_password='$pass'"; 
+        $sql = "SELECT * FROM customer WHERE customer_id='$customer_id' AND customer_password='$customer_passsword'"; 
  
         $result = mysqli_query($conn, $sql); 
  
         if (mysqli_num_rows($result) === 1) { 
             $row = mysqli_fetch_assoc($result); 
-            if ($row['manager_email'] === $uname && $row['manager_password'] === $pass) { 
+            if ($row['customer_id'] === $customer_id && $row['customer_password'] === $customer_passsword) { 
                 echo "Logged in!"; 
-                $_SESSION['manager_email'] = $row['manager_email']; 
-                $_SESSION['manager_password'] = $row['manager_password']; 
-                $_SESSION['manager_name'] = $row['manager_name']; 
-                $_SESSION['manager_id'] = $row['manager_id']; 
-                header("Location: home.php"); 
+                $_SESSION['customer_id'] = $row['customer_name']; 
+                $_SESSION['customer_address'] = $row['customer_phone']; 
+                $_SESSION['customer_password'];
+                header("Location: Menupage.php"); 
                 exit(); 
  
             } 
